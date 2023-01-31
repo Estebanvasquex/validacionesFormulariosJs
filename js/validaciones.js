2c1bd3217bf6d4437d8e56e3e4ef5dc3cd9f36be
estebanvasquex <estebanvasquex@gmail.com>
2023-01-30 23:12:38 -0500

    Se finaliza la validación del formulario ingresando clases al input requerido

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,45 +1,101 @@
 
-const inputNacimiento = document.querySelector("#birth");
-
-inputNacimiento.addEventListener("blur", (evento)=>{
-    validarNacimiento(evento.target);
-    
-
-    //console.log(evento.target)
 
 
-})
+// la funcion Valida se llama cada que el usuario salga del input haga un blur
+export function valida(input) {
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
 
-function validarNacimiento(input){
-    const fechaCliente = new Date (input.value);
-    let mensaje = "";
-    if (!mayorDeEdad(fechaCliente)){
-        mensaje = "Debes tener al menos 18 años de edad";
-        console.log(mensaje);
-    };
 
-    /*Define el mensaje de validación personalizado para el elemento seleccionado con el mensaje especifico*/ 
-    input.setCustomValidity(mensaje);
-
-    console.log("fecha cliente: " + fechaCliente);
-
+  // Debido a que la clase la tiene el div padre y no el input sedebe usar input.parentElement.classList.remove, con esta validacion estamos agregando la clase que muestra el input en rojo cuando no cumple, también se está usando el objeto validity y su propiedad valid con esto sabemos si los datos ingresados en el input despues de la validación son correctos
+  
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = "";
+  } else {
+    input.parentElement.classList.add("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML =
+      mostrarMensajeDeError(tipoDeInput, input);
+  }
 }
 
-function mayorDeEdad(fecha){
-    const fechaActual = new Date();
-    const diferenciaFechas = new Date(
-        fecha.getUTCFullYear()+18,
-        fecha.getUTCMonth(),
-        fecha.getUTCDate()
-       
-    );
+const tipoDeErrores = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError",
+];
 
-    /*Al realizar la comparación entre la fecha de nacimiento sumando 18 años esta debe ser menor o igual a la fecha actual en caso de que de una fecha mayor a fecha actual quiere decir que en ese año cumple los 18*/
-    return diferenciaFechas <= fechaActual;
+const mensajesDeError = {
+  nombre: {
+    valueMissing: "El campo nombre no puede estar vacío",
+  },
+  email: {
+    valueMissing: "El campo correo no puede estar vacío",
+    typeMismatch: "El correo no es válido",
+  },
+  password: {
+    valueMissing: "El campo contraseña no puede estar vacío",
+    patternMismatch:
+      "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
+  },
+  nacimiento: {
+    valueMissing: "Este campo no puede estar vacío",
+    customError: "Debes tener al menos 18 años de edad",
+  },
+  numero: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "El formato requerido es XXXXXXXXXX 10 números",
+  },
+  direccion: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "La dirección debe contener entre 10 a 40 caracteres.",
+  },
+  ciudad: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "La ciudad debe contener entre 10 a 40 caracteres.",
+  },
+  estado: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "El estado debe contener entre 10 a 40 caracteres.",
+  },
+};
 
-    
+const validadores = {
+  nacimiento: (input) => validarNacimiento(input),
+};
 
+function mostrarMensajeDeError(tipoDeInput, input) {
+  let mensaje = "";
+  tipoDeErrores.forEach((error) => {
+    if (input.validity[error]) {
+      console.log(tipoDeInput, error);
+      console.log(input.validity[error]);
+      console.log(mensajesDeError[tipoDeInput][error]);
+      mensaje = mensajesDeError[tipoDeInput][error];
+    }
+  });
+  return mensaje;
 }
 
+function validarNacimiento(input) {
+  const fechaCliente = new Date(input.value);
+  let mensaje = "";
+  if (!mayorDeEdad(fechaCliente)) {
+    mensaje = "Debes tener al menos 18 años de edad";
+  }
 
+  input.setCustomValidity(mensaje);
+}
 
+function mayorDeEdad(fecha) {
+  const fechaActual = new Date();
+  const diferenciaFechas = new Date(
+    fecha.getUTCFullYear() + 18,
+    fecha.getUTCMonth(),
+    fecha.getUTCDate()
+  );
+  return diferenciaFechas <= fechaActual;
+}
